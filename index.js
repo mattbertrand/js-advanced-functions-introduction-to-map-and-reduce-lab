d974948336d405a955c5740a6b82cea23158725c
github username <github email address>
2021-03-01 17:01:18 -0800

    commit tree b2ca30caea325ea8bd35687667ec5035fb6aedb5

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+function mapToNegativize(src) {
+    let n = []
+    for (let i=0; i<src.length; i++) {
+        n.push(-1 * src[i])
+    }
+    return n
+} 
+
+function mapToNoChange(src) {
+    let n = []
+    for (let i=0; i<src.length; i++) {
+        n.push(src[i])
+    }
+    return n
+}
+
+function mapToDouble(src) {
+    let n = []
+    for (let i=0; i<src.length; i++) {
+        n.push(src[i] * 2)
+    }
+    return n
+}
+function mapToSquare(src) {
+    let n = []
+    for (let i=0; i<src.length; i++) {
+        n.push(src[i] * src[i])
+    }
+    return n
+}
+
+function reduceToTotal(src, startingPoint=0) {
+    let total = startingPoint;
+    for (let i=0; i<src.length; i++) {
+        total = total + src[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(src) {
+    for (let i=0; i<src.length; i++) {
+        if (!src[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(src) {
+    for (let i=0; i<src.length; i++) {
+        if (src[i]) return true
+    }
+    return false
+}
